c695a769102897bb7ad2e2deaceb4a21446aac97
AdamPhilipSmith <adamphilipsmith@hotmail.co.uk>
2020-06-16 18:31:46 +0100

    Searcher working. tf Ranking working

--- src/Search/Main.java
+++ src/Search/Main.java
@@ -1,9 +1,10 @@
 package Search;
 
 import java.io.IOException;
-import java.util.HashSet;
+import java.util.*;
 
 import static Search.ReadFile.results;
+import static Search.ReadFile.sitesAndWords;
 import static Search.Search.Search;
 
 public class Main {
@@ -12,11 +13,45 @@ public class Main {
 
         //Crawler crawler = new Crawler("https://www.mmu.ac.uk/", 1000);
         //crawler.crawl();
-        ReadFile test = new ReadFile("mmuSiteTest1000.txt");
-        HashSet results2 = Search("Adam", results);
+        ReadFile test = new ReadFile("mmuSiteTest100.txt");
+        HashSet results2 = Search("Applications", results);
 
-        System.out.println(results2);
 
+        //HashSet allWordsOnSite = Search("https://www.mmu.ac.uk/business-school/business/sme-support/fuel-cell-innovation-centre", wordResults);
+
+        ArrayList tester = new ArrayList();
+        tester = (ArrayList) sitesAndWords.get("https://www.mmu.ac.uk/international/fees-and-funding");
+        double wordCount = 0;
+        double searchedWordCount = 0;
+        for (Object results : tester) {
+            String searchedWord = results.toString();
+            if (searchedWord.equalsIgnoreCase("University")) {
+                System.out.println("found");
+                searchedWordCount++;
+            }
+            wordCount++;
+        }
+        double rankScore = searchedWordCount / wordCount;
+        System.out.println(rankScore);
+        System.out.println(wordCount);
+        System.out.println(tester);
+        List<String> words = (List<String>) sitesAndWords.get("https://www.mmu.ac.uk/international/fees-and-funding");
+
+        System.out.println(tf(words, "University"));
+
+
+
+
+        /*for( String word: allWordsOnSite){
+            if (word.equals("and)")){
+                System.out.println("count");
+            }
+        }*/
+
+
+        //System.out.println(results2);
+        //System.out.println(allWordsOnSite);
+        //System.out.println(sitesAndWords);
       /* for (Object key: results2.keySet()) {
            if (key.equals("research")) {
                System.out.println(get(key));
@@ -25,9 +60,20 @@ public class Main {
 */
 
 
+    }
+
+
+        public static double tf(List<String> doc, String term) {
+            double result = 0;
+            for (String word : doc) {
+                if (term.equalsIgnoreCase(word))
+                    result++;
+            }
+            return result / doc.size();
         }
         //System.out.println);
     }
+
 
 
 
--- src/Search/ReadFile.java
+++ src/Search/ReadFile.java
@@ -4,14 +4,16 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 
 public class ReadFile {
 
     public static LinkedHashMap results = new LinkedHashMap<String, HashSet>();
-
-
+    public static HashMap sitesAndWords = new HashMap<String, ArrayList>();
+    public static LinkedHashMap wordResults = new LinkedHashMap<String, HashSet>();
 
 
     public ReadFile(String filename) throws IOException {
@@ -34,8 +36,16 @@ public class ReadFile {
                 if (word.startsWith("*PAGE:")) { //checks if the word is a URL. If so, sets the URL and continues on to the next word
 
                     Url = word.substring(6);
-                    String[] _arr = Url.split("\\s"); // Removes the occasional time a website still has extra text after its URL for some reason.
+
+                    String[] _arr = Url.split("\\s"); // Removes the occasional time a website still has extra text after its URL for some reason. //TODO Maybe mention this in write up maybe (had problems and fixed them.)
                     Url = _arr[0];
+
+
+                    if (Url.substring(Url.length() - 1).equals("/")) { // Gets rid of the "/" at the end of some URL's preventing duplicate results.//TODO same as above
+                        Url = Url.substring(0, Url.length() - 1);
+                    }
+
+
                     continue;
                 }
 
@@ -43,12 +53,17 @@ public class ReadFile {
 
                     continue;
                 }
+               //ArrayList<String> words = (ArrayList<String>) sitesAndWords.get(Url);
 
-                addEntryToHashMap(results, word, Url); //adds the word to the Hasmap with the corresponding Url.
+                //if (words == null) {
 
+                //}
+                //System.out.println(words);
+                addEntryToHashMap(results, word, Url); //adds the word to the Hashmap with the corresponding Url.
+                addToSiteAndWords(Url, word);
             }
             file.close();
-        }catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
@@ -56,22 +71,75 @@ public class ReadFile {
 
 
     //TODO sort the code below
+
     /**
      * @param hashMap
      * @param word
-     * @param url
-     * Adds the word as the key and the url as the value of the hashmap. Checks for duplicates inside each UrlList and only appends
-     * new urls to the end.
+     * @param url     Adds the word as the key and the url as the value of the hashmap. Checks for duplicates inside each UrlList and only appends
+     *                new urls to the end.
      */
     protected static void addEntryToHashMap(LinkedHashMap hashMap, String word, String url) { // Adds searched word as key and url as a value of the hashmap. Checks for duplicates only adding new Url if previous doesn't exist.
-        // System.out.println(word);
+        //System.out.println(word);
         HashSet<String> urlHashMap = (HashSet) hashMap.get(word);
-        //System.out.println("4");
+        //System.out.println(hashMap.get(word));
+        //System.out.println(urlHashMap);
 
-        if (urlHashMap == null) { //Checks a
+        if (urlHashMap == null) { //Checks to see if this Url has already been added for this word.
             HashSet<String> hashSet = new HashSet<String>();
             hashSet.add(url);
             hashMap.put(word, hashSet);
-        } else { urlHashMap.add(url); }
+        } else {
+            //System.out.println(url);
+            //System.out.println(urlHashMap);
+            //TODO might cause problems
+            if (urlHashMap.contains(url)) {
+                urlHashMap.add(url);
+            }
+            urlHashMap.add(url);
+
+            //System.out.println(urlHashMap);
+        }
     }
-}
+
+    protected static void addToSiteAndWords(String url, String word) { // Adds searched word as key and url as a value of the hashmap. Checks for duplicates only adding new Url if previous doesn't exist.
+        //System.out.println(word);
+        //HashSet<String> wordHashMap = (HashSet) hashMap.get(url);
+        //System.out.println(hashMap.get(word));
+        //System.out.println(urlHashMap);
+
+
+        ArrayList temp = new ArrayList();
+        temp = (ArrayList) sitesAndWords.get(url);
+
+        if (temp!=null) {
+            temp.add(word);
+            //System.out.println(1);
+        }
+        //TODO Below shouldn't be necessary and yet it breaks if it isn't here??? System never prints '2' so looks like it isn't used.
+        else{
+            //System.out.println(2);
+            ArrayList temp2 = new ArrayList();
+            temp2.add(word);
+            temp = temp2;
+            sitesAndWords.put(url, temp2);
+
+        }
+
+        sitesAndWords.put(url, temp);
+        //System.out.println(sitesAndWords);
+        //System.out.println("/////////////////////////////////////////");
+
+       /* if (wordHashMap == null) { //Checks to see if this word has already been added for this word.
+            HashSet<String> hashSet2 = new HashSet<String>();
+            hashSet2.add(word);
+            hashMap.put(url, hashSet);
+        } else {*/
+
+           // wordHashMap.add(word);
+
+
+            //System.out.println(urlHashMap);
+        }
+    }
+
+
