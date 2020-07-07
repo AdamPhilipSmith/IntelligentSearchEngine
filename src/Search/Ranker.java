e813c874da4814001ca72a191d16ee05b90771ac
AdamPhilipSmith <adamphilipsmith@hotmail.co.uk>
2020-07-07 17:05:30 +0100

    Tidied Ranker. 'OR' search still not working

--- src/Search/Ranker.java
+++ src/Search/Ranker.java
@@ -6,104 +6,84 @@ import static Search.Indexer.sitesToWords;
 
 public class Ranker {
 
-    //TODO Check over code
+    //TODO Code refactored and changed, Could do with a once over.
     //TODO maybe use a stop list https://stackoverflow.com/questions/30028519/information-retrieval-how-to-combine-different-word-results-when-using-tf-idf
 
-    public static double tf(List<String> doc, String term) {
+    //Goes through the given web pages, calculating how many times the given word appears
+    //Divides by the total size of each web page to normalise the result
+    public static double tf(List<String> page, String searchedWord) {
         double result = 0;
-        for (String word : doc) {
-            if (term.equalsIgnoreCase(word))
+        for (String word : page) {
+            if (searchedWord.equalsIgnoreCase(word))
                 result++;
         }
-
-        return result / doc.size(); //normalises the frequency to avoid a bias towards larger documents
+        return result / page.size();
     }
 
-    public static double idf(HashMap<String, ArrayList<String>> docs, String term) {
+    //Goes through the entirety of the given website, checking how common the given searched word is
+    //Stops common words being weighted as highly as less common ones
+    public static double idf(HashMap<String, ArrayList<String>> indexedSite, String searchedWord) {
         double n = 0;
-        Iterator it = docs.entrySet().iterator();
+        Iterator it = indexedSite.entrySet().iterator();
         while (it.hasNext()) {
             Map.Entry pair = (Map.Entry) it.next();
             List<String> text = (List<String>) pair.getValue();
-            //System.out.println(text);
 
             for (String word : text) {
-                //System.out.println(word);
-                //System.out.println(text);
-                if (term.equalsIgnoreCase(word)) {
-                    //System.out.println("yes");
+                if (searchedWord.equalsIgnoreCase(word)) {
                     n++;
                     break;
                 }
             }
-
-
         }
-        //System.out.println("Documents:" + n);
-        //System.out.println("Size:" + docs.size());
+
         //TODO maybe use math.log10. Google it
-        return Math.log(docs.size() / n);
+        return Math.log(indexedSite.size() / n);
+    }
+    // Combines the tf and the idf to give a tf-idf rating
+    public static double tfIdf(List<String> page, HashMap<String, ArrayList<String>> indexedSite, String term) {
+
+        return tf(page, term) * idf(indexedSite, term);
+
     }
 
-    public static double tfIdf(List<String> doc, HashMap<String, ArrayList<String>> docs, String term) {
-        //System.out.println(tf(doc, term) * idf(docs, term));
-        return tf(doc, term) * idf(docs, term);
-
-    }
-
-    public static Map<Double,String> displayRankedResults(HashSet searchResults, String searchedWords) {
+    //Determines the tf-idf for all websites returned in the search results, combining their scores for each word, ranking them and then returning them in descending order
+    public static Map<Double, String> displayRankedResults(HashSet searchResults, String searchedWords) {
 
         Map<Double, String> sortedURLs = new HashMap<Double, String>();
 
-        String[] splitWords = searchedWords.split(" "); // Separates the different words from the search term
+        // Separates the different words from the search term
+        String[] splitWords = searchedWords.split(" ");
 
-        TreeMap<Double, String> rankedURLs = new TreeMap<>(); //Tree map used since it will automatically sort the results by the key
+        //Tree map used since it will automatically sort the results by the key
+        TreeMap<Double, String> rankedURLs = new TreeMap<>();
 
-
-
+        //TODO this breaks 'OR' search
         if (searchResults != null) {
             for (Object url : searchResults) {
                 List<String> words2 = (List<String>) sitesToWords.get(url);
                 double rank = 0;
                 for (int i = 0; i < splitWords.length; i++) {
                     //TODO check that adding Tf IDF together is the best way of doing this
-                    rank += tfIdf(words2, sitesToWords, splitWords[i]); //Goes through each of the searched terms for each website, getting the Tf IDF rank to get a combined TF IDF score for each site
-
+                    //Goes through each of the searched terms for each website, getting the Tf IDF rank to get a combined TF IDF score for each site
+                    rank += tfIdf(words2, sitesToWords, splitWords[i]);
 
                 }
                 String stringURL = url.toString();
+
                 rankedURLs.put(rank, stringURL);
-                //System.out.println(words2.size());
-                //System.out.println("tf:" + tf(words2, searchedWords));
-                //System.out.println("idf=" + idf(sitesToWords, searchedWords));
-
-                //System.out.print("Rank:");
-
-
-                // System.out.println("");
             }
-            sortedURLs = rankedURLs.descendingMap(); //Since the results are ordered lowest to highest, it needs to be reversed to get the best results at the top
+            //Since the results are ordered lowest to highest, it needs to be reversed to get the best results at the top
+            sortedURLs = rankedURLs.descendingMap();
 
-
-            System.out.println(sortedURLs);
+            //TODO below no longer needed?
             Iterator it = sortedURLs.entrySet().iterator();
             while (it.hasNext()) {
                 Map.Entry pair = (Map.Entry) it.next();
                 System.out.println(pair.getValue());
-
-                //leave below for now but it isn't used
-                //List<Double> URL = (List<Double>) pair.getKey();
+                System.out.println(pair.getKey());
             }
         }
-
-    /*public class reverseRank implements Comparator<String>{
-
-        @Override
-        public int compare(String str1, String str2) {
-            return str1.compareTo(str2);
-        }
-
-    }*/
 
         return sortedURLs;
     }
